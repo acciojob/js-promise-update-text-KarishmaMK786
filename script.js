5971b9bac7dca6106931d1aa2f3fdd459b74d1da
KarishmaMK786 <130684724+KarishmaMK786@users.noreply.github.com>
2023-06-26 10:38:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function generatePromise() {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve("Hello, World!")
+		} ,1000)
+	}
+}
+generatePromise().then((data) => {
+const element = document.getElementById("output");
+element.innerText = data;
+})
